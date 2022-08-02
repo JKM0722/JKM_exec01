3cf9cbc32a5990a1d3c2d7e7c1195cf05fbed984
JKM0722 <108040612+JKM0722@users.noreply.github.com>
2022-08-02 23:50:35 +0900

    e

--- 001/js/swiper.js
+++ 001/js/swiper.js
@@ -3,16 +3,22 @@ window.addEventListener("load", () => {
 
   
   var swiper = new Swiper(".Swipe1", {
-    slidesPerView: "auto",
+    slidesPerView: 1,
     spaceBetween: 30,
     pagination: {
       el: ".sec2 .swiper-pagination",
       clickable: true,
+      loop:false,
     },
     navigation: {
       nextEl: ".rb",
       prevEl: ".lb"
-    }
+    },
+    breakpoints: { //반응형 조건 속성
+      768: { //768 이상일 경우
+        slidesPerView: "auto", //레이아웃 2열
+      },
+    }///breakpoints
   });
   
 
