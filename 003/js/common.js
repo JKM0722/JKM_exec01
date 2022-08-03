ea3826a7891f20f453cef3ab62e6b3d7a7226d9d
JKM0722 <108040612+JKM0722@users.noreply.github.com>
2022-08-03 14:26:29 +0900

    44

--- 003/js/common.js
+++ 003/js/common.js
@@ -1,49 +1,13 @@
 //common.js
 
-window.addEventListener("wheel", function(e){
-	e.preventDefault();
-},{passive : false});
-
-// var mHtml = $("html");
-// var page = 1;
-
-// mHtml.animate({scrollTop : 0},10);
-
-// $(window).on("wheel", function(e) {
-//     if(mHtml.is(":animated")) return;
-//     if(e.originalEvent.deltaY > 0) {
-//         if(page == 5) return;
-//         page++;
-//     } else if(e.originalEvent.deltaY < 0) {
-//         if(page == 1) return;
-//         page--;
-//     }
-//     var posTop =(page-1) * $(window).height();
-//     mHtml.animate({scrollTop : posTop});
-// })
-
-var $html = $("html");
-var page = 1;
-var lastPage = $(".section").length;
-
-$html.animate({scrollTop:0},10);
+$(()=>{
+    $('#fullpage').fullpage({
+        slidesNavigation: true,
+        responsiveWidth: 1024,
+        autoScrolling: true,
+        scrollBar: false
+    });
 
 
-$(window).on("wheel", function(e){
- 
-	if($html.is(":animated")) return;
- 
-	if(e.originalEvent.deltaY > 0){
-		if(page== lastPage) return;
- 
-		page++;
-	}else if(e.originalEvent.deltaY < 0){
-		if(page == 1) return;
- 
-		page--;
-	}
-	var posTop = (page-1) * $(window).height();
- 
-	$html.animate({scrollTop : posTop});
- 
-});
+
+})//////////JQB///////////////
