0f00ec31edebdeb639ceb04c35db46f98c2b1dc2
JKM0722 <108040612+JKM0722@users.noreply.github.com>
2022-08-05 14:48:48 +0900

    ee

--- 003/js/common.js
+++ 003/js/common.js
@@ -8,19 +8,20 @@ $(()=>{
         scrollBar: false
     });
 
-    $(function () {
-        // 메뉴 버튼 꾸밈
-        $(".bar").click(function () {
-            $(this).toggleClass("open");
-            $(".gnb")
-            .fadeToggle(200);
 
-            $(".gnb li").each((idx,ele)=>{
-                $(ele).delay(100*idx).animate({top:"0",opacity:1})
-            });
-            
-            if(!$(this).is("open")) $(".gnb li").attr("style","");    
+    // 메뉴 버튼 꾸밈
+    $(".bar").click(function () {
+
+        $(this).toggleClass("open");
+        $(".gnb")
+        .fadeToggle(200);
+
+        $(".gnb li").each((idx,ele)=>{
+            $(ele).stop(true,true).delay(100*idx).animate({top:"0",opacity:1})
         });
+        
+        // if (!$(".bar").is("open")) {$(".gnb li").attr("style","")}
+        if(!$(this).is("open")) $(".gnb li").attr("style",'');    
     });
 
 
