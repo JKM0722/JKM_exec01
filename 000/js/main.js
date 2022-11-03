afc2a63c861a8b768746b7832677a821bb8ca0fe
omg <108040612+JKM0722@users.noreply.github.com>
2022-11-03 15:34:32 +0900

    d

--- 000/js/main.js
+++ 000/js/main.js
@@ -170,6 +170,26 @@ $(document).ready(function () {
     }
       });
 
+      var swiper = new Swiper(".mySwiper", {
+        spaceBetween: 30,
+        centeredSlides: true,
+        slidesPerView: 3,
+        loop: true,
+        autoplay: {
+          delay: 2500,
+          disableOnInteraction: false,
+          
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
+
 
 
 }) ////////////jQB
